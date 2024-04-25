103fa4997f40e483bd2876a73ee95057745ee522
Kees Jan Koster <kjkoster@kjkoster.org>
2024-04-25 23:29:44 +0600

    Add validation request handling to Slack bot.

--- slackbot/slackbot.py
+++ slackbot/slackbot.py
@@ -1,12 +1,42 @@
 import os
+import hmac
 import slack
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 
+SLACKBOT_SIGNING_SECRET = os.getenv('SLACKBOT_SIGNING_SECRET')
+
+# https://stackoverflow.com/questions/64341222/how-to-validate-slack-api-request
+@app.route('/slack-validation', methods=['GET', 'POST']) 
+def slack_validation():
+    headers = request.headers
+    timestamp = request.headers['X-Slack-Request-Timestamp'] 
+
+    slack_payload = request.form
+    dict_slack = slack_payload.to_dict()
+
+### This is the key that solved the issue for me, where urllib.parse.quote(val, safe='')] ###
+    payload= "&".join(['='.join([key, urllib.parse.quote(val, safe='')]) for key, val in dict_slack.items()])  
+
+    ### compose the message:
+    sig_basestring = 'v0:' + timestamp + ':' + payload
+
+    sig_basestring = sig_basestring.encode('utf-8')
+
+    ### secret
+    signing_secret = SLACKBOT_SIGNING_SECRET.encode('utf-8') # I had an env variable declared with slack_signing_secret
+    
+    my_signature = 'v0=' + hmac.new(signing_secret, sig_basestring, hashlib.sha256).hexdigest()
+    print('my signature: ')
+    print(my_signature)
+    
+    return '', 200
+
+
 slack_client = slack.WebClient(token=os.getenv('SLACKBOT_TOKEN'))
-slack_events = SlackEventAdapter(os.getenv('SLACKBOT_SIGNING_SECRET') ,'/slack/events', app)
+slack_events = SlackEventAdapter(SLACKBOT_SIGNING_SECRET ,'/slack/events', app)
 
 @slack_events.on('message')
 def on_message(payload):

980e94a88d0e7ffb86a51e5f2907c7d841bcedad
Kees Jan Koster <kjkoster@kjkoster.org>
2024-04-26 21:35:28 +0600

    Directly connect to Ollama from the Slack bot. We'll put a message queue in between later.

--- slackbot/slackbot.py
+++ slackbot/slackbot.py
@@ -2,10 +2,62 @@ import os
 import re
 import logging
 from typing import Callable
+
 from slack_sdk import WebClient
 from slack_bolt import App, Say, BoltContext
 
+from ollama import Client
+ROLE_USER='user'
+ROLE_SYSTEM='system'
+ROLE_ASSISTANT='assistant'
+MSG_MESSAGE='message'
+MSG_ROLE='role'
+MSG_CONTENT='content'
+MSG_RESPONSE='response'
+MSG_STATUS='status'
+MSG_SUCCESS='success'
+
 logging.basicConfig(level=logging.DEBUG)
+
+# --- Assistant section
+
+class LLM:
+    def __init__(self, chat_model, image_model):
+        llm_client = Client(host=f"http://{os.getenv("OLLAMA_HOST")}:{os.getenv{OLLAMA_PORT}")
+        self.chat_model = chat_model
+        pull_response = llm_client.pull(self.chat_model)
+        if pull_response[MSG_STATUS] != MSG_SUCCESS:
+            raise Exception(f"failed to pull {self.chat_model}: {pull_response}")
+
+        self.image_model = image_model
+        pull_response = llm_client.pull(self.image_model)
+        if pull_response[MSG_STATUS] != MSG_SUCCESS:
+            raise Exception(f"failed to pull {self.image_model}: {pull_response}")
+
+        self.messages = []
+        self.append_message(ROLE_SYSTEM, "You are a Kenyan Extension Officer, specialised in agriculture. You cannot answer questions that are not about agriculture.")
+
+    def chat(self, content):
+        self.append_message(ROLE_USER, content)
+        response = llm_client.chat(model=self.chat_model, messages=self.messages)
+        message = response[MSG_MESSAGE]
+        self.append_message(message[MSG_ROLE], message[MSG_CONTENT])
+        return message[MSG_CONTENT]
+
+    def check_image(self, image):
+        self.append_message(ROLE_USER, f"{IMAGE_PROMPT}: {image}")
+        response = llm_client.generate(model=self.image_model, prompt=IMAGE_PROMPT, images=[image])
+        message = response[MSG_RESPONSE]
+        self.append_message(ROLE_ASSISTANT, message)
+        return f"I looked at {image}. {message}"
+
+    def append_message(self, role, content):
+        self.messages.append({MSG_ROLE: role, MSG_CONTENT: content})
+        # print(f"------- {self.messages}")
+
+llm = LLM('mistral', 'llava')
+
+# --- Slack section
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
           signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
@@ -14,16 +66,6 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
 def log_request(logger: logging.Logger, body: dict, next: Callable):
     logger.debug(body)
     return next()
-
-
-@app.message("test")
-def reply_to_test(say):
-    say("Yes, tests are important!")
-
-
-@app.message(re.compile("bug"))
-def mention_bug(say):
-    say("Do you mind filing a ticket?")
 
 
 # middleware function
@@ -38,8 +80,13 @@ def extract_subtype(body: dict, context: BoltContext, next: Callable):
 @app.event({"type": "message", "subtype": None})
 def reply_in_thread(body: dict, say: Say):
     event = body["event"]
+    logger.info(event)
+
+    text = body["event"]["text"]
+    llm_response = llm.chat(text)
+
     thread_ts = event.get("thread_ts", None) or event["ts"]
-    say(text="Hey, what's up?", thread_ts=thread_ts)
+    say(text=llm_response, thread_ts=thread_ts)
 
 
 @app.event(

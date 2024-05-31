6f440d02948810f1ab8aa22279dbd0dc0d42abc6
dedenbangkit <mail@dedenbangkit.com>
2024-05-31 17:23:41 +0700

    [#7] Modify backend run comment

--- backend/prod.sh
+++ backend/prod.sh
@@ -3,5 +3,5 @@ set -eu
 pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt
 
-# Run the FastAPI server in port 8000
+# Run the FastAPI server in port 5000, only accessible from docker network
 fastapi run main.py

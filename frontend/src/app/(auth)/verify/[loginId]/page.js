d1b95e7adf939f046eb924a24fffa1382f45994c
wayangalihpratama <wgprtm@gmail.com>
2024-06-04 20:11:49 +0800

    [#11] Add loading page

--- frontend/src/app/(auth)/verify/[loginId]/page.js
+++ frontend/src/app/(auth)/verify/[loginId]/page.js
@@ -30,11 +30,22 @@ const VerifyLogin = ({ params }) => {
           },
         });
         route.replace("/");
-      }, 500);
+      }, 1500);
     }
   }, [auth.isLogin]);
 
-  return <div>Verify {params.loginId}</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite] text-blue-300"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Verifying...
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default VerifyLogin;
--- frontend/src/app/loading.js
+++ frontend/src/app/loading.js
@@ -1,5 +1,16 @@
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] text-blue-300"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+          Loading...
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;

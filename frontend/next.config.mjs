918c87d19606e5429c761eb4f864faf2c658192e
wayangalihpratama <wgprtm@gmail.com>
2024-06-04 16:45:33 +0800

    [#11] Rewrite /login into Home n create verify/<login_id> route

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,9 +5,13 @@ const env = dotenv.config();
 const backend_port = env.parsed.BACKEND_PORT;
 
 const nextConfig = {
-  output: "export",
+  // output: "export",
   async rewrites() {
     return [
+      {
+        source: "/login",
+        destination: "/",
+      },
       {
         source: "/api/:path*",
         destination: `http://backend:${backend_port}/api/:path*`, // Proxy to Backend
---
+++ frontend/src/app/verify/[loginId]/page.js
@@ -0,0 +1,5 @@
+const VerifyLogin = ({ params }) => {
+  return <div>Verify {params.loginId}</div>;
+};
+
+export default VerifyLogin;

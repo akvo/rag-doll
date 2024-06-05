97c80a6f5dc4132c34392895d34d10cc82930094
wayangalihpratama <wgprtm@gmail.com>
2024-06-05 18:02:38 +0800

    [#11] Fix chat window responsive

--- frontend/src/components/chats/ChatWindow.js
+++ frontend/src/components/chats/ChatWindow.js
@@ -11,7 +11,18 @@ const ChatWindow = () => {
   const [message, setMessage] = useState("");
   const [aiMessages, setAiMessages] = useState([
     "AI suggested message...",
-    "AI suggested message...",
+    <>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget
+      sollicitudin augue. Curabitur quis risus ut nulla consectetur gravida.
+      Nunc sit amet turpis tempor, rutrum mi vel, molestie purus. Lorem ipsum
+      dolor sit amet, consectetur adipiscing elit. In hac habitasse platea
+      dictumst. Donec dignissim mi ut eros elementum fringilla. Phasellus
+      maximus feugiat nunc. Aliquam erat volutpat. Cras dictum tortor bibendum
+      velit dapibus, in rutrum mi porttitor. In malesuada odio at augue
+      efficitur maximus. Nullam elementum est a sagittis consectetur. Nullam ac
+      sem dolor. Pellentesque quis sagittis augue. Quisque lobortis sed eros ut
+      dignissim. Curabitur fringilla hendrerit dui, vitae consequat dolor
+    </>,
   ]);
 
   const handleInput = (event) => {
@@ -77,9 +88,9 @@ const ChatWindow = () => {
       </div>
 
       {/* Messages */}
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-3/4">
         {/* User Messages */}
-        <div className="flex-1 p-4 overflow-y-scrol">
+        <div className="flex-1 p-4 overflow-auto border-b">
           <div className="flex mb-4">
             <div className="relative bg-white p-4 rounded-lg shadow-lg max-w-xs md:max-w-md">
               <div className="absolute bottom-0 left-0 w-0 h-0 border-t-8 border-t-white border-l-8 border-l-transparent border-b-0 border-r-8 border-r-transparent transform translate-x-1/2 translate-y-1/2"></div>
@@ -91,15 +102,28 @@ const ChatWindow = () => {
           <div className="flex mb-4 justify-end">
             <div className="relative bg-green-500 text-white p-4 rounded-lg shadow-lg max-w-xs md:max-w-md">
               <div className="absolute bottom-0 right-0 w-0 h-0 border-t-8 border-t-green-500 border-r-8 border-r-transparent border-b-0 border-l-8 border-l-transparent transform -translate-x-1/2 translate-y-1/2"></div>
-              <p>Hello! How are you?</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                eget sollicitudin augue. Curabitur quis risus ut nulla
+                consectetur gravida. Nunc sit amet turpis tempor, rutrum mi vel,
+                molestie purus. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. In hac habitasse platea dictumst. Donec
+                dignissim mi ut eros elementum fringilla. Phasellus maximus
+                feugiat nunc. Aliquam erat volutpat. Cras dictum tortor bibendum
+                velit dapibus, in rutrum mi porttitor. In malesuada odio at
+                augue efficitur maximus. Nullam elementum est a sagittis
+                consectetur. Nullam ac sem dolor. Pellentesque quis sagittis
+                augue. Quisque lobortis sed eros ut dignissim. Curabitur
+                fringilla hendrerit dui, vitae consequat dolor.
+              </p>
               <p className="text-right text-xs text-gray-200 mt-2">10:01 AM</p>
             </div>
           </div>
         </div>
 
         {/* AI Messages */}
-        <div className="flex-1 p-4 overflow-y-scroll">
-          <div className="bg-gray-400 p-4 h-full rounded">
+        <div className="flex-1 p-4 overflow-auto mb-2">
+          <div className="bg-gray-100 p-4 h-full rounded-lg shadow-inner overflow-auto">
             {aiMessages.map((aiMessage, index) => (
               <div key={index} className="flex mb-4">
                 <div className="relative bg-blue-500 text-white p-4 rounded-lg shadow-lg w-full">
@@ -115,7 +139,7 @@ const ChatWindow = () => {
       </div>
 
       {/* Input */}
-      <div className="p-4 bg-white border-t flex items-center mb-[65px]">
+      <div className="flex p-4 bg-white border-t items-center fixed bottom-16 w-full">
         <textarea
           ref={textareaRef}
           value={message}

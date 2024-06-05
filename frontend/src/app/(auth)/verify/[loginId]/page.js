2788b9ca05d0718028ddc770767688e1af818352
wayangalihpratama <wgprtm@gmail.com>
2024-06-05 18:18:27 +0800

    [#11] Update verified route into chats instead of home page

--- frontend/src/app/(auth)/verify/[loginId]/page.js
+++ frontend/src/app/(auth)/verify/[loginId]/page.js
@@ -29,7 +29,7 @@ const VerifyLogin = ({ params }) => {
             email: "test@test.com  ",
           },
         });
-        route.replace("/");
+        route.replace("/chats");
       }, 1500);
     }
   }, [auth.isLogin]);
@@ -37,7 +37,7 @@ const VerifyLogin = ({ params }) => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite] text-blue-300"
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite] text-green-500"
         role="status"
       >
         <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">

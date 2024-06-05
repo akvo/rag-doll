58fa079dcf1b6d6aabf165e23d8842bb8c4ba96d
wayangalihpratama <wgprtm@gmail.com>
2024-06-05 17:16:04 +0800

    [#11] Split chat tab button into component

---
+++ frontend/src/components/chats/ChatTabButton.js
@@ -0,0 +1,84 @@
+"use client";
+
+import { useChatContext, useChatDispatch } from "@/context/ChatContextProvider";
+
+const chatButtonType = (type) => {
+  const { clientId } = useChatContext();
+  const chatDispatch = useChatDispatch();
+
+  const handleOnClickChat = () => {
+    chatDispatch({
+      type: "UPDATE",
+      payload: {
+        clientId: null,
+      },
+    });
+  };
+
+  switch (type) {
+    case "chats":
+      return {
+        active: clientId ? false : true,
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z"
+            />
+          </svg>
+        ),
+        text: "Chats",
+        onClick: handleOnClickChat,
+      };
+    case "reference":
+      return {
+        active: false,
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+            />
+          </svg>
+        ),
+        text: "Reference",
+        onClick: () => {},
+      };
+    default:
+      throw Error(`Unknown tab type: ${type}.`);
+  }
+};
+
+const ChatTabButton = ({ type }) => {
+  const buttonEl = chatButtonType(type);
+
+  return (
+    <button
+      className={`flex flex-col items-center px-4 py-2 ${
+        buttonEl.active && type ? "text-green-500" : "text-gray-600"
+      }  hover:text-green-600`}
+      onClick={() => (buttonEl?.onClick ? buttonEl.onClick() : null)}
+    >
+      {buttonEl.icon}
+      {buttonEl.text}
+    </button>
+  );
+};
+
+export default ChatTabButton;
--- frontend/src/components/chats/ChatTabs.js
+++ frontend/src/components/chats/ChatTabs.js
@@ -1,63 +1,12 @@
-"use client";
-
-import { useChatContext, useChatDispatch } from "@/context/ChatContextProvider";
+import ChatTabButton from "./ChatTabButton";
 
 const ChatTabs = () => {
-  const { clientId } = useChatContext();
-  const chatDispatch = useChatDispatch();
-
-  const handleOnClickChat = () => {
-    chatDispatch({
-      type: "UPDATE",
-      payload: {
-        clientId: null,
-      },
-    });
-  };
-
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-center items-center space-x-20 h-16">
-          <button
-            className={`flex flex-col items-center px-4 py-2 ${
-              !clientId && "text-green-500"
-            }  hover:text-green-600`}
-            onClick={handleOnClickChat}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z"
-              />
-            </svg>
-            Chats
-          </button>
-          <button className="flex flex-col items-center px-4 py-2 text-gray-600 hover:text-green-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-              />
-            </svg>
-            Reference
-          </button>
+          <ChatTabButton type="chats" />
+          <ChatTabButton type="reference" />
         </div>
       </div>
     </div>

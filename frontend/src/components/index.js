b120325466b216a8ac8b0cd502b220c654e9dd61
wayangalihpratama <wgprtm@gmail.com>
2024-06-06 11:53:32 +0800

    [#11] Create notification components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from "./layout/Header";
 export { default as ChatWindow } from "./chats/ChatWindow";
 export { default as ChatList } from "./chats/ChatList";
 export { default as ChatTabs } from "./chats/ChatTabs";
+export { default as Notification } from "./utils/Notification";

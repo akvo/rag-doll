a0565d802d97c41b6b7be01634eae304aff50994
dedenbangkit <mail@dedenbangkit.com>
2024-06-17 11:18:14 +0700

    [#18] Initial backend ci setup

---
+++ backend/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pip -q install --upgrade pip
+pip -q install --cache-dir=.pip -r requirements.txt
+
+echo "Running tests"
+COVERAGE_PROCESS_START=./.coveragerc \
+  coverage run --parallel-mode --concurrency=thread,gevent --rcfile=./.coveragerc \
+  /usr/local/bin/pytest -vvv -rP
+
+echo "Coverage"
+coverage combine --rcfile=./.coveragerc
+coverage report -m --rcfile=./.coveragerc
+
+if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
+  coveralls
+fi
+
+./storage.sh clear
+flake8
---
+++ ci/build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2039
+#shellcheck disable=SC3040
+
+set -exuo pipefail
+
+IMAGE_PREFIX="eu.gcr.io/akvo-lumen/idh-idc"
+CI_COMMIT=$(git rev-parse --short "$GITHUB_SHA")
+
+echo "CI_COMMIT=${CI_COMMIT}"
+
+dc() {
+    docker compose \
+        --ansi never \
+        -f docker-compose.yml \
+        -f docker-compose.test.yml \
+        "$@"
+}
+
+backend_build() {
+
+    cp ./setup.cfg ./backend/setup.cfg
+    docker build \
+        --tag "${IMAGE_PREFIX}/backend:latest" \
+        --tag "${IMAGE_PREFIX}/backend:${CI_COMMIT}" backend
+}
+
+cp .env.template .env
+
+backend_build
+docker compose \
+    -f docker-compose.yml \
+    -f docker-compose.test \
+    run -T backend ./test.sh

1b77f41a1c995e17a70ca4247aececd23558bbf6
Deden Bangkit <dedenbangkit@users.noreply.github.com>
2024-06-18 15:06:19 +0700

    [#18] Initial backend ci setup (#20)
    * [#18] Initial backend ci setup
    
    * [#18] Add flake8
    
    * [#18] Modify branch for backend test
    
    * [#18] Modify test workflows
    
    * [#18] Fix image name
    
    * [#18] Fix build.sh
    
    * [#18] Modify build.sh and dedicate compose test
    
    * [#18] Fix docker compose test volumes
    
    * [#18] Remove unused CLI
    
    * [#18] Add setup.cfg to docker compose

---
+++ ci/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC2039
+#shellcheck disable=SC3040
+
+set -exuo pipefail
+
+IMAGE_PREFIX="eu.gcr.io/akvo-lumen/agriconnect"
+CI_COMMIT=$(git rev-parse --short "$GITHUB_SHA")
+
+echo "CI_COMMIT=${CI_COMMIT}"
+
+backend_build() {
+
+    cp ./setup.cfg ./backend/setup.cfg
+    docker build \
+        --tag "${IMAGE_PREFIX}/backend:latest" \
+        --tag "${IMAGE_PREFIX}/backend:${CI_COMMIT}" backend
+}
+
+cp env.template .env
+
+backend_build
+docker compose \
+    -f docker-compose.test.yml \
+    run -T backend ./test.sh

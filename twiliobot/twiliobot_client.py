13fa88dd1ccd8220e36593546efc44b8ca7e99e2
wayangalihpratama <wgprtm@gmail.com>
2024-06-18 11:49:00 +0800

    [#14] Refactor twiliobot client into class

--- twiliobot/twiliobot_client.py
+++ twiliobot/twiliobot_client.py
@@ -1,71 +1,77 @@
 import os
 import json
 import logging
+import time
+from json.decoder import JSONDecodeError
+from twilio.base.exceptions import TwilioRestException
 
 from twilio.rest import Client
 
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+class TwiliobotClient:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
 
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_WHATSAPP_FROM = 'whatsapp:+14155238886'  # Consider externalizing
+        self.TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+        self.TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+        self.TWILIO_WHATSAPP_FROM = "whatsapp:+14155238886"
 
-twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        self.twilio_client = Client(
+            self.TWILIO_ACCOUNT_SID,
+            self.TWILIO_AUTH_TOKEN)
+
+    def chunk_text_by_paragraphs(self, text: str, max_length: int) -> list[str]:
+        """
+        Split text into chunks by paragraphs,
+        ensuring each chunk is within the max_length.
+        """
+        paragraphs = text.split("\n\n")
+        chunks = []
+
+        for paragraph in paragraphs:
+            if len(paragraph) <= max_length:
+                chunks.append(paragraph)
+            else:
+                for i in range(0, len(paragraph), max_length):
+                    chunk = paragraph[i:i + max_length]
+                    chunks.append(chunk)
+
+        return chunks
+
+    def send_whatsapp_message(self, message_body: bytes) -> None:
+        """
+        Consume messages from the RabbitMQ queue and send them via Twilio.
+        """
+        try:
+            self.logger.info(f"Message received: {message_body}")
+            queue_message = json.loads(message_body.decode("utf8"))
+            text = queue_message["text"]
+            phone = queue_message["to"]["phone"]
+
+            chunks = self.chunk_text_by_paragraphs(text, 1500)
+            self.logger.info(f"sending '{text}' to WhatsApp number {phone}")
+            for chunk in chunks:
+                self.logger.info(f"sending '{chunk}' to WhatsApp")
+                self.logger.info(f"number {phone}")
+                response = self.twilio_client.messages.create(
+                    from_=self.TWILIO_WHATSAPP_FROM,
+                    body=chunk,
+                    to=f"whatsapp:{phone}",
+                )
+                if response.error_code is not None:
+                    self.logger.error(
+                        f"Failed to send message to WhatsApp number "
+                        f"{phone}: {response.error_message}"
+                    )
+                time.sleep(0.5)  # 500ms delay
+
+        except JSONDecodeError as e:
+            self.logger.error(f"Error decoding JSON message: {e}")
+        except TwilioRestException as e:
+            self.logger.error(f"Error sending message to Twilio: {e}")
+        except Exception as e:
+            self.logger.error(f"Unexpected error: {e}")
 
 
-def chunk_text_by_paragraphs(text, max_length):
-    """
-    Split text into chunks by paragraphs,
-    ensuring each chunk is within the max_length.
-    """
-    paragraphs = text.split('\n\n')
-    chunks = []
-    current_chunk = ""
-
-    for paragraph in paragraphs:
-        # +2 for the \n\n
-        if len(current_chunk) + len(paragraph) + 2 <= max_length:
-            if current_chunk:
-                current_chunk += "\n\n"
-            current_chunk += paragraph
-        else:
-            if current_chunk:
-                chunks.append(current_chunk)
-            current_chunk = paragraph
-
-    if current_chunk:
-        chunks.append(current_chunk)
-    return chunks
-
-
-def twilio_send_runner(ch, method, properties, body) -> None:
-    """
-    Consume messages from the RabbitMQ queue and send them via Twilio.
-    """
-    try:
-        logger.info(f"Message received: {body}")
-        queue_message = json.loads(body.decode('utf8'))
-        text = queue_message['text']
-        phone = queue_message['to']['phone']
-        if '+' not in str(phone):
-            phone = f"+{phone}"
-
-        # Chunk the text by paragraphs
-        chunks = chunk_text_by_paragraphs(text, 1500)
-        logger.info(f"sending '{text}' to WhatsApp number {phone}")
-        for chunk in chunks:
-            logger.info(f"sending '{chunk}' to WhatsApp number {phone}")
-            response = twilio_client.messages.create(
-                from_=TWILIO_WHATSAPP_FROM,
-                body=chunk,
-                to=f"whatsapp:{phone}"
-            )
-            if response.error_code is not None:
-                logger.error(
-                    f"Failed to send message to WhatsApp number {phone}: {
-                        response.error_message}")
-
-    except Exception as e:
-        logger.error(f"Error while sending message to Twilio: {e}")
+twiliobot_client = TwiliobotClient()

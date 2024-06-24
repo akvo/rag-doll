158484d79e1f43306eeec3f1274852e2ebdedb57
wayangalihpratama <wgprtm@gmail.com>
2024-06-24 15:56:53 +0800

    [#19] Add more test into user routes

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,3 +1,5 @@
+import re
+
 from sqlalchemy import Column, String, BigInteger
 from sqlmodel import Field, SQLModel
 
@@ -8,6 +10,19 @@ class User(SQLModel, table=True):
         sa_column=Column(BigInteger, unique=True),
     )
     login_code: str | None = None
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.phone_number = self._sanitize_phone_number(
+            data.get('phone_number'))
+
+    def _sanitize_phone_number(self, phone_number):
+        if isinstance(phone_number, int):
+            return phone_number
+        if not re.match(r'^\+\d+$', phone_number):
+            raise ValueError("Phone number contains invalid characters")
+        phone_number_digits = re.sub(r'\D', '', phone_number)
+        return int(phone_number_digits)
 
     # return phone number in international format
     def __str__(self) -> str:

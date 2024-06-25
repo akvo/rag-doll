b2815a10f464d62af775552f034c216df67ed65a
wayangalihpratama <wgprtm@gmail.com>
2024-06-25 18:56:50 +0800

    [#11] Set default phone number country

--- frontend/src/app/(auth)/(with-auth-layout)/login/page.js
+++ frontend/src/app/(auth)/(with-auth-layout)/login/page.js
@@ -80,6 +80,8 @@ const Login = () => {
               value={phoneNumber}
               onChange={setPhoneNumber}
               international
+              defaultCountry="KE"
+              initialValueFormat="international"
               className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6"
             />
           </div>

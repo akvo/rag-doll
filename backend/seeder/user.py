fa38d5e625f926deb1f3a43c02cf2d8277aaff70
wayangalihpratama <wgprtm@gmail.com>
2024-06-27 13:49:53 +0800

    [#25] Fix raise err on user seeder

--- backend/seeder/user.py
+++ backend/seeder/user.py
@@ -1,5 +1,4 @@
 import re
-import os
 
 from faker import Faker
 from models.user import User, User_Properties
@@ -76,8 +75,8 @@ def interactive_seeder(session: Session):
             print(f"Seeded user {user.id} with phone number {phone_number}")
         except ValidationError as e:
             print(f"Validation error for phone number {phone_number}: {e}")
-            if os.getenv("TESTING"):
-                raise e
+            session.close()
+            raise e
     finally:
         session.close()
         print("Seeder process completed.")

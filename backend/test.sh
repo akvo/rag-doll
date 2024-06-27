0d25a197efbdb47893fa622a53002a068bd030d6
wayangalihpratama <wgprtm@gmail.com>
2024-06-27 16:49:35 +0800

    [#25] Try to start app in test.sh

--- backend/test.sh
+++ backend/test.sh
@@ -7,10 +7,23 @@ pip -q install --cache-dir=.pip -r requirements.txt
 
 pip -q install --cache-dir=.pip /lib/Akvo_rabbitmq_client
 
+# Read backend port from environment variable
+BACKEND_PORT=${BACKEND_PORT}
+
+# Start the FastAPI server in the background and capture the process ID
+uvicorn your_fastapi_app:app --host 0.0.0.0 --port $BACKEND_PORT &
+FASTAPI_PID=$!
+
+# Give the server some time to start
+sleep 5
+
 echo "Running tests"
 COVERAGE_PROCESS_START=./.coveragerc \
   coverage run --parallel-mode --concurrency=thread,gevent --rcfile=./.coveragerc \
   /usr/local/bin/pytest -vvv -rP
+
+# Shut down the FastAPI server
+kill $FASTAPI_PID
 
 echo "Coverage"
 coverage combine --rcfile=./.coveragerc

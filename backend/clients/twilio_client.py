1dcc37cb2129b767b457e13e5bf6deb98ad43d4c
wayangalihpratama <wgprtm@gmail.com>
2024-07-03 09:59:35 +0800

    [#27] Move twilio_client into backend

---
+++ backend/clients/twilio_client.py
@@ -0,0 +1,138 @@
+import os
+import json
+import logging
+import time
+import phonenumbers
+
+from datetime import datetime
+from json.decoder import JSONDecodeError
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+from pydantic import BaseModel, ValidationError
+from pydantic_extra_types.phone_numbers import PhoneNumber
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class IncomingMessage(BaseModel):
+    MessageSid: str
+    From: str
+    Body: str
+    NumMedia: int
+
+
+class PhoneValidationModel(BaseModel):
+    phone: PhoneNumber
+
+
+class TwilioClient:
+    def __init__(self):
+        self.TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+        self.TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+        self.TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER")
+        if os.getenv("TESTING"):
+            # only for testing purpose
+            self.TWILIO_WHATSAPP_NUMBER = "+12345678911"
+        self.TWILIO_WHATSAPP_FROM = f"whatsapp:{self.TWILIO_WHATSAPP_NUMBER}"
+
+        self.twilio_client = Client(
+            self.TWILIO_ACCOUNT_SID,
+            self.TWILIO_AUTH_TOKEN)
+
+    def chunk_text_by_paragraphs(self, text: str, max_length: int) -> list[str]:
+        """
+        Split text into chunks by paragraphs,
+        ensuring each chunk is within the max_length.
+        """
+        paragraphs = text.split("\n\n")
+        chunks = []
+        for paragraph in paragraphs:
+            if len(paragraph) <= max_length:
+                chunks.append(paragraph)
+            else:
+                for i in range(0, len(paragraph), max_length):
+                    chunk = paragraph[i:i + max_length]
+                    chunks.append(chunk)
+        return chunks
+
+    def send_whatsapp_message(self, body: str) -> None:
+        """
+        Consume messages from the RabbitMQ queue and send them via Twilio.
+        """
+        try:
+            logger.info(f"Message received: {body}")
+            queue_message = json.loads(body)
+            text = queue_message["text"]
+            phone = queue_message["to"]["phone"]
+
+            chunks = self.chunk_text_by_paragraphs(text, 1500)
+            logger.info(f"sending '{text}' to WhatsApp number {phone}")
+            for chunk in chunks:
+                logger.info(f"sending '{chunk}' to WhatsApp")
+                logger.info(f"number {phone}")
+                response = self.twilio_client.messages.create(
+                    from_=self.TWILIO_WHATSAPP_FROM,
+                    body=chunk,
+                    to=f"whatsapp:{phone}",
+                )
+                if response.error_code is not None:
+                    logger.error(
+                        f"Failed to send message to WhatsApp number "
+                        f"{phone}: {response.error_message}"
+                    )
+                time.sleep(0.5)  # 500ms delay
+
+        except JSONDecodeError as e:
+            logger.error(f"Error decoding JSON message: {e}")
+        except TwilioRestException as e:
+            logger.error(f"Error sending message to Twilio: {e}")
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+
+    def validate_and_format_phone_number(self, phone_number: str) -> str:
+        try:
+            parsed_number = phonenumbers.parse(phone_number)
+            if not phonenumbers.is_valid_number(parsed_number):
+                raise ValueError(f"Invalid phone number: {phone_number}")
+            # Format the phone number
+            formatted_number = phonenumbers.format_number(
+                parsed_number, phonenumbers.PhoneNumberFormat.E164)
+            # Validate the formatted phone number using the Pydantic model
+            PhoneValidationModel(phone=formatted_number)
+            return formatted_number
+        except phonenumbers.phonenumberutil.NumberParseException as e:
+            raise ValueError(f"Phone number parsing error: {e}")
+        except ValidationError as e:
+            raise ValidationError(f"Phone number validation error: {e}")
+
+    def format_to_queue_message(self, values: dict) -> str:
+        """
+        Consume messages from Twilio then format into queue message.
+        """
+        iso_timestamp = datetime.now().isoformat()
+        try:
+            # Validate and format the phone number
+            formatted_phone = self.validate_and_format_phone_number(
+                phone_number=values['From'])
+            queue_message = {
+                'id': values['MessageSid'],
+                'timestamp': iso_timestamp,
+                'platform': 'WHATSAPP',
+                'from': {
+                    'phone': formatted_phone,
+                },
+                'text': values['Body'],
+                'media': []
+            }
+            # Add media files if present
+            num_media = values.get('NumMedia', 0)
+            for i in range(num_media):
+                media_url = values.get(f'MediaUrl{i}', '')
+                if media_url:
+                    queue_message['media'].append(media_url)
+            return json.dumps(queue_message)
+        except ValueError as e:
+            logger.error(f"Error formatting message: {e}")
+            raise
---
+++ backend/tests/clients/test_twiliobot_client.py
@@ -0,0 +1,180 @@
+import json
+import pytest
+from unittest.mock import patch, MagicMock
+from twilio.base.exceptions import TwilioRestException
+from twilio.rest import Client
+from clients.twilio_client import TwilioClient
+
+
+@pytest.fixture
+def twilio_client():
+    return TwilioClient()
+
+
+def test_chunk_text_by_paragraphs(twilio_client):
+    text = "This is a test paragraph.\n\nThis is another test paragraph that is"
+    text += " longer than the previous one to check chunking functionality."
+    max_length = 50
+    chunks = twilio_client.chunk_text_by_paragraphs(text, max_length)
+    assert len(chunks) == 3
+    assert chunks[0] == "This is a test paragraph."
+    assert chunks[1] == "This is another test paragraph that is longer than"
+    assert chunks[2] == " the previous one to check chunking functionality."
+
+
+def test_validate_and_format_phone_number_valid(twilio_client):
+    phone_number = "+6281234567890"
+    formatted_phone = twilio_client.validate_and_format_phone_number(
+        phone_number)
+    assert formatted_phone == "+6281234567890"
+
+
+def test_validate_and_format_phone_number_invalid(twilio_client):
+    phone_number = "1234567890"  # Invalid format
+    with pytest.raises(ValueError):
+        twilio_client.validate_and_format_phone_number(phone_number)
+
+
+def test_validate_and_format_phone_number_parse_error(twilio_client):
+    phone_number = "invalid_number"  # Invalid number format
+    with pytest.raises(ValueError):
+        twilio_client.validate_and_format_phone_number(phone_number)
+
+
+def test_format_to_queue_message_valid(twilio_client):
+    values = {
+        'MessageSid': '1234567890',
+        'From': '+6281234567890',
+        'Body': 'Test message',
+        'NumMedia': 1,
+        'MediaUrl0': 'http://example.com/image.jpg'
+    }
+    formatted_message = twilio_client.format_to_queue_message(values)
+    assert isinstance(formatted_message, str)
+    queue_message = json.loads(formatted_message)
+    assert queue_message['id'] == '1234567890'
+    assert queue_message['from']['phone'] == '+6281234567890'
+    assert queue_message['text'] == 'Test message'
+    assert queue_message['media'] == ['http://example.com/image.jpg']
+    assert 'timestamp' in queue_message
+    assert 'platform' in queue_message
+
+
+def test_format_to_queue_message_missing_fields(twilio_client):
+    values = {
+        'From': '+6281234567890',
+        'Body': 'Test message',
+    }
+    with pytest.raises(KeyError):
+        twilio_client.format_to_queue_message(values)
+
+
+def test_format_to_queue_message_invalid_phone_number(twilio_client):
+    values = {
+        'MessageSid': '1234567890',
+        'From': 'invalid_number',  # Invalid phone number format
+        'Body': 'Test message',
+        'NumMedia': 0,
+    }
+    with pytest.raises(ValueError, match="Phone number parsing error: "):
+        twilio_client.format_to_queue_message(values)
+
+
+@patch.object(Client, 'messages', new_callable=MagicMock)
+@patch('clients.twilio_client.logger')
+def test_send_whatsapp_message_success(
+    mock_logger,
+    mock_messages,
+    twilio_client
+):
+    message_body = json.dumps({
+        "text": "Hello, this is a test message.",
+        "to": {"phone": "+1234567890"}
+    }).encode()
+
+    mock_messages.create.return_value = MagicMock(error_code=None)
+
+    twilio_client.send_whatsapp_message(message_body)
+
+    assert twilio_client.TWILIO_WHATSAPP_FROM == "whatsapp:+12345678911"
+    assert mock_messages.create.called
+    mock_messages.create.assert_called_with(
+        from_=twilio_client.TWILIO_WHATSAPP_FROM,
+        body="Hello, this is a test message.",
+        to="whatsapp:+1234567890",
+    )
+
+
+@patch.object(Client, 'messages', new_callable=MagicMock)
+@patch('clients.twilio_client.logger')
+def test_send_whatsapp_message_twilio_error(
+    mock_logger,
+    mock_messages,
+    twilio_client
+):
+    message_body = json.dumps({
+        "text": "Hello, this is a test message.",
+        "to": {"phone": "+1234567890"}
+    }).encode()
+
+    mock_messages.create.side_effect = TwilioRestException(
+        status=400, uri='http://test.uri', msg="Twilio error")
+
+    twilio_client.send_whatsapp_message(message_body)
+
+    assert mock_messages.create.called
+    mock_messages.create.assert_called_with(
+        from_=twilio_client.TWILIO_WHATSAPP_FROM,
+        body="Hello, this is a test message.",
+        to="whatsapp:+1234567890",
+    )
+
+    # Check that the error was logged
+    mock_logger.error.assert_any_call(
+        "Error sending message to Twilio: HTTP 400 error: Twilio error")
+
+
+@patch.object(Client, 'messages', new_callable=MagicMock)
+@patch('clients.twilio_client.logger')
+def test_send_whatsapp_message_json_decode_error(
+    mock_logger,
+    mock_messages,
+    twilio_client
+):
+    message_body = b"invalid json"
+
+    twilio_client.send_whatsapp_message(message_body)
+
+    assert not mock_messages.create.called
+
+    # Check that the error was logged
+    mock_logger.error.assert_any_call(
+        "Error decoding JSON message: Expecting value: line 1 column 1 (char 0)"
+    )
+
+
+@patch.object(Client, 'messages', new_callable=MagicMock)
+@patch('clients.twilio_client.logger')
+def test_send_whatsapp_message_unexpected_error(
+    mock_logger,
+    mock_messages,
+    twilio_client
+):
+    message_body = json.dumps({
+        "text": "Hello, this is a test message.",
+        "to": {"phone": "+1234567890"}
+    }).encode()
+
+    mock_messages.create.side_effect = Exception("Unexpected error")
+
+    twilio_client.send_whatsapp_message(message_body)
+
+    assert mock_messages.create.called
+    mock_messages.create.assert_called_with(
+        from_=twilio_client.TWILIO_WHATSAPP_FROM,
+        body="Hello, this is a test message.",
+        to="whatsapp:+1234567890",
+    )
+
+    # Check that the error was logged
+    mock_logger.error.assert_any_call("Unexpected error: Unexpected error")

a0db4c36dd932ef425b419c51229e613d9963cee
wayangalihpratama <wgprtm@gmail.com>
2024-07-15 08:54:02 +0800

    [#34] Use socket session manager to save user phone number when sio connect & get when listen even on chats

--- backend/core/socketio_config.py
+++ backend/core/socketio_config.py
@@ -28,11 +28,11 @@ cookie = SimpleCookie()
 
 @sio_server.on("connect")
 async def sio_connect(sid, environ):
-    # TODO :: We can use AUTH_TOKEN in websocket
-    # should we implement this?
     cookie.load(environ["HTTP_COOKIE"])
     auth_token = cookie["AUTH_TOKEN"].value
     user_phone_number = verify_jwt_token(auth_token).get("uphone_number")
+    async with sio_server.session(sid) as session:
+        session["user_phone_number"] = user_phone_number
     logger.info(f"A user sid[{sid}] connected, {user_phone_number}")
 
 
@@ -44,11 +44,15 @@ async def sio_disconnect(sid):
 @sio_server.on("chats")
 async def chat_message(sid, msg):
     """Receive a user chat message from FE and put in chat replies queue"""
-    logger.info(f"Server received: sid[{sid}] msg: {msg}")
-    await rabbitmq_client.producer(
-        body=json.dumps(msg),
-        routing_key=RABBITMQ_QUEUE_USER_CHAT_REPLIES,
-    )
+    async with sio_server.session(sid) as session:
+        user_phone_number = session["user_phone_number"]
+        logger.info(
+            f"Server received: sid[{sid}] msg: {msg}, {user_phone_number}"
+        )
+        await rabbitmq_client.producer(
+            body=json.dumps(msg),
+            routing_key=RABBITMQ_QUEUE_USER_CHAT_REPLIES,
+        )
 
 
 async def user_chats_callback(body: str):

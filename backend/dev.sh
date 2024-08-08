05d20313a83fd3fa49fbc05bfad97911006f341e
wayangalihpratama <wgprtm@gmail.com>
2024-08-08 14:19:18 +0800

    [#41] Prettier & refine assistant code

--- backend/dev.sh
+++ backend/dev.sh
@@ -2,10 +2,10 @@
 set -eu
 
 # Install FFmpeg to support pydub
-apt-get update
-apt-get install -y flac ffmpeg
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+# apt-get update
+# apt-get install -y flac ffmpeg
+# apt-get clean
+# rm -rf /var/lib/apt/lists/*
 
 pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt

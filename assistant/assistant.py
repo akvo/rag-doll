fa465ce22a6a83f95034ffd310f28ede6a52b96b
Wayan Galih Pratama <49269977+wayangalihpratama@users.noreply.github.com>
2024-08-08 17:52:16 +0800

    [#41] Prettier & refine assistant code (#42)
    * [#41] Prettier & refine assistant code
    
    * [#41] Make assistant log more readable
    
    * [#41] Add assistant replies queue and update assistant reply route
    
    * [#41] Update assistant reply message format

--- assistant/assistant.py
+++ assistant/assistant.py
@@ -3,9 +3,12 @@ import json
 import asyncio
 import logging
 import chromadb
+
 from time import sleep
 from openai import OpenAI
 from Akvo_rabbitmq_client import rabbitmq_client
+from datetime import datetime, timezone
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,26 +21,40 @@ CHROMADB_PORT: int = os.getenv('CHROMADB_PORT')
 CHROMADB_COLLECTION: str = os.getenv('CHROMADB_COLLECTION')
 CHROMADB_DISTANCE_CUTOFF: float = float(os.getenv("CHROMADB_DISTANCE_CUTOFF"))
 
-def connect_to_chromadb(host: str, port: int, collection_name: str) -> chromadb.Collection:
+
+def connect_to_chromadb(
+    host: str, port: int, collection_name: str
+) -> chromadb.Collection:
     '''
-        Connect to ChromaDB. The ChromaDB service takes a second or so to start,
-        so we have a crude retry loop. Once connected, we look up or create the
-        collection.
+        Connect to ChromaDB. The ChromaDB service takes a
+        second or so to start, so we have a crude retry loop.
+        Once connected, we look up or create the collection.
     '''
     chromadb_client = None
-    while chromadb_client == None:
+    while chromadb_client is None:
+        url = f"http://{host}:{port}/{collection_name}"
         try:
-            logger.info(f"trying http://{host}:{port}/{collection_name}...")
-            chromadb_client = chromadb.HttpClient(host=host, port=port, settings=chromadb.Settings(anonymized_telemetry=False))
+            logger.info(f"trying {url}...")
+            chromadb_client = chromadb.HttpClient(
+                host=host,
+                port=port,
+                settings=chromadb.Settings(anonymized_telemetry=False)
+            )
             return chromadb_client.get_or_create_collection(collection_name)
         except Exception as e:
-            logger.warning(f"unable to connect to http://{host}:{port}/{collection_name}, retrying...: {type(e)}: {e}")
+            logger.warning(
+                f"unable to connect to {url}, retrying...: {type(e)}: {e}")
             chromadb_client = None
             sleep(1)
 
 
-def query_collection(collection: chromadb.Collection, prompt: str) -> list[str]:
-    logger.info(f"    -> will query: {collection} for {prompt}, with cut-off {CHROMADB_DISTANCE_CUTOFF}")
+def query_collection(
+    collection: chromadb.Collection, prompt: str
+) -> list[str]:
+    logger.info(
+        f"[ASSISTANT] -> will query: {collection} for {prompt}"
+        f" with cut-off {CHROMADB_DISTANCE_CUTOFF}"
+    )
 
     query_result = collection.query(
         query_texts=[prompt],
@@ -45,22 +62,31 @@ def query_collection(collection: chromadb.Collection, prompt: str) -> list[str]:
         include=["documents", "distances"]
     )
     filtered_documents = [
-        doc for doc, dist in zip(query_result["documents"][0], query_result["distances"][0])
+        doc for doc, dist in zip(
+            query_result["documents"][0],
+            query_result["distances"][0]
+        )
         if dist < CHROMADB_DISTANCE_CUTOFF
     ]
 
-    logger.info(f"    -> accepted {len(filtered_documents)} of {len(query_result["documents"][0])} query results: distances: {query_result["distances"][0]}, cut-off: {CHROMADB_DISTANCE_CUTOFF}")
-    return filtered_documents
+    logger.info(
+        f"[ASSISTANT] -> accepted {len(filtered_documents)} of "
+        f"{len(query_result["documents"][0])} query results: distances:"
+        f"{query_result["distances"][0]}, cut-off: {CHROMADB_DISTANCE_CUTOFF}"
+    )
+    return filtered_documents, query_result
 
 
-chromadb_collection = connect_to_chromadb(CHROMADB_HOST, CHROMADB_PORT, CHROMADB_COLLECTION)
+chromadb_collection = connect_to_chromadb(
+    CHROMADB_HOST, CHROMADB_PORT, CHROMADB_COLLECTION
+)
 
 # --- LLM section
 
-ASSISTANT_ROLE    = os.getenv("ASSISTANT_ROLE")
+ASSISTANT_ROLE = os.getenv("ASSISTANT_ROLE")
 OPENAI_CHAT_MODEL = os.getenv("OPENAI_CHAT_MODEL")
-RAG_PROMPT        = os.getenv("RAG_PROMPT")
-RAGLESS_PROMPT    = os.getenv("RAGLESS_PROMPT")
+RAG_PROMPT = os.getenv("RAG_PROMPT")
+RAGLESS_PROMPT = os.getenv("RAGLESS_PROMPT")
 
 
 class LLM:
@@ -75,10 +101,13 @@ class LLM:
         response = self.llm_client.chat.completions.create(
             model=self.chat_model, messages=self.messages
         )
-        logger.info(f"OPENAI RESPONSE: {response}")
+        logger.info(f"[ASSISTANT] -> OPENAI RESPONSE: {response}")
         message = response.choices[0].message
         self.append_message(message.role, message.content)
-        return message.content
+        return {
+            "message": message.content,
+            "created_at": response.created
+        }
 
     def append_message(self, role, content):
         self.messages.append({"role": role, "content": str(content)})
@@ -89,66 +118,82 @@ llm = LLM(OPENAI_CHAT_MODEL)
 
 # --- RabbitMQ Section
 
-RABBITMQ_QUEUE_USER_CHATS        = os.getenv("RABBITMQ_QUEUE_USER_CHATS")
-RABBITMQ_QUEUE_USER_CHAT_REPLIES = os.getenv("RABBITMQ_QUEUE_USER_CHAT_REPLIES")
+RABBITMQ_QUEUE_USER_CHATS = os.getenv("RABBITMQ_QUEUE_USER_CHATS")
+RABBITMQ_QUEUE_ASSISTANT_CHAT_REPLIES = os.getenv(
+    "RABBITMQ_QUEUE_ASSISTANT_CHAT_REPLIES"
+)
 
 
 def queue_message_and_llm_response_to_reply(
     queue_message: dict, llm_response: dict
 ) -> str:
-    reply = {
-        "id": f"{queue_message['id']}-reply",
-        "timestamp": f"{llm_response['created_at'][:-1]}+00:00",  # noqa replace 'Z' with +00:00
-        "platform": queue_message["platform"],
-        "to": queue_message["from"],
-        "text": llm_response["message"]["content"],
-    }
-    return json.dumps(reply)
+    try:
+        logger.info(f"[ASSISTANT] -> Formatting message: {queue_message}")
+        conversation_envelope = queue_message.get('conversation_envelope', {})
+        timestamp = float(llm_response["created_at"])
+        iso_timestamp = datetime.fromtimestamp(
+            timestamp, tz=timezone.utc
+        ).isoformat()
+        reply = {
+            "conversation_envelope": {
+                "message_id": conversation_envelope.get('message_id'),
+                "client_phone_number": conversation_envelope.get(
+                    "client_phone_number"
+                ),
+                "user_phone_number": conversation_envelope.get(
+                    "user_phone_number"
+                ),
+                "sender_role": "assistant",
+                "platform": conversation_envelope.get("platform"),
+                "timestamp": iso_timestamp
+            },
+            "body": llm_response["message"],
+            "media": [],
+            "context": [],
+            "transformation_log": [llm_response["message"]]
+        }
+        logger.info(f"[ASSISTANT] -> Message ready: {reply}")
+        return json.dumps(reply)
+
+    except Exception as e:
+        logger.error(f"[ASSISTANT] -> Error formatting message {type(e)}: {e}")
 
 
-def publish_reliably(queue_message: str) -> None:
-    global user_chat_reply_queue
-
-    retries = 5
-    while retries > 0:
-        retries = retries - 1
-
-        try:
-            user_chat_reply_queue.basic_publish(
-                exchange="",
-                routing_key=RABBITMQ_QUEUE_USER_CHAT_REPLIES,
-                body=queue_message,
-            )
-            return
-        except Exception as e:
-            logger.warning(f"{type(e)}: {e}")
-
-        sleep(5)
-        user_chat_reply_queue = connect_and_create_queue(
-            RABBITMQ_QUEUE_USER_CHAT_REPLIES
+async def publish_reliably(queue_message: str) -> None:
+    try:
+        logger.info(f"[ASSISTANT] -> Sending message: {queue_message}")
+        await rabbitmq_client.producer(
+            body=queue_message,
+            routing_key=RABBITMQ_QUEUE_ASSISTANT_CHAT_REPLIES,
         )
+    except Exception as e:
+        logger.error(f"[ASSISTANT] -> Error send to queue {type(e)}: {e}")
 
 
 async def on_message(body: str) -> None:
-    logger.info(f"    -> message: {body}")
+    logger.info(f"[ASSISTANT] -> message received: {body}")
     from_client = json.loads(body)
 
     # query the knowledge base for RAG context
-    rag_context = query_collection(chromadb_collection, from_client["body"])
+    rag_context, query_result = query_collection(
+        chromadb_collection, from_client["body"]
+    )
 
     # if there is context, add it to the prompt
     if len(rag_context) > 0:
-        prompt = RAG_PROMPT.format(from_client['body'], collection_query_result)
+        prompt = RAG_PROMPT.format(
+            from_client['body'], query_result
+        )
     else:
         prompt = RAGLESS_PROMPT.format(from_client['body'])
 
     # then send that prompt over to the LLM
     llm_response = llm.chat(prompt)
-    logger.info(f"LLM replied: {llm_response}")
-
-    publish_reliably(
-        queue_message_and_llm_response_to_reply(from_client, llm_response)
+    logger.info(f"[ASSISTANT] -> LLM replied: {llm_response}")
+    reply_message = queue_message_and_llm_response_to_reply(
+        queue_message=from_client, llm_response=llm_response
     )
+    await publish_reliably(queue_message=reply_message)
 
 
 async def main():
@@ -164,9 +209,9 @@ async def main():
         while True:
             await asyncio.sleep(1)
     except KeyboardInterrupt:
-        logger.info("Shutting down...")
+        logger.info("[ASSISTANT] -> RabbitMQ Shutting down...")
         await rabbitmq_client.disconnect()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
-

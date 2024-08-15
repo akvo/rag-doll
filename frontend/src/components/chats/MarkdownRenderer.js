953d6dabac4a86c610b972a72fbf3a04f6acc7bc
wayangalihpratama <wgprtm@gmail.com>
2024-08-15 14:22:25 +0800

    [#48] Handle `\n` new line in markdown renderer

--- frontend/src/components/chats/MarkdownRenderer.js
+++ frontend/src/components/chats/MarkdownRenderer.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 
 const MarkdownRenderer = ({ index, content, className }) => {
+  if (content.trim() === "") {
+    // add new line
+    return <br />;
+  }
+
   return (
     <div key={index} className={`prose ${className}`}>
       <ReactMarkdown>{content}</ReactMarkdown>

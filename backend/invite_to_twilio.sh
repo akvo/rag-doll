0b3fed96a75ed2acfbee72674945e5f15678733a
wayangalihpratama <wgprtm@gmail.com>
2024-08-19 17:03:22 +0800

    [#58] Improve invite_to_twilio.sh to support multiple numbers

--- backend/invite_to_twilio.sh
+++ backend/invite_to_twilio.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Parameters
-TO_PHONE_NUMBER=$1
+# How to use: ./invite_to_twilio.sh "phone_number1" "phone_number2" "phone_number_etc"
+
+# Default message
 MESSAGE_BODY="Hi! To register your phone number with Twilio, please reply to this message with: join fly-handle."
 
 # Twilio API endpoint and credentials
@@ -10,9 +11,14 @@ TWILIO_AUTH_TOKEN=${TWILIO_AUTH_TOKEN}
 TWILIO_WHATSAPP_NUMBER=${TWILIO_WHATSAPP_NUMBER}
 FROM_PHONE_NUMBER="whatsapp:$TWILIO_WHATSAPP_NUMBER"
 
-# Send WhatsApp message using Twilio API
-curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages.json" \
---data-urlencode "To=whatsapp:$TO_PHONE_NUMBER" \
---data-urlencode "From=$FROM_PHONE_NUMBER" \
---data-urlencode "Body=$MESSAGE_BODY" \
--u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
+# Loop through each phone number provided as arguments
+for TO_PHONE_NUMBER in "$@"; do
+    # Send whatsapp message
+    curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages.json" \
+    --data-urlencode "To=whatsapp:$TO_PHONE_NUMBER" \
+    --data-urlencode "From=$FROM_PHONE_NUMBER" \
+    --data-urlencode "Body=$MESSAGE_BODY" \
+    -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
+
+    echo "Message sent to $TO_PHONE_NUMBER"
+done

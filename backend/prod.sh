0f97de550a0c164c6297cde328f254e5450ae355
Anjar Fiandrianto <anjar@akvo.org>
2024-08-30 10:55:15 +0700

    Deploy Ragdoll to Agriconnect

--- backend/prod.sh
+++ backend/prod.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-# Install FFmpeg to support pydub
-apt-get update
-apt-get install -y flac ffmpeg
-apt-get clean
-rm -rf /var/lib/apt/lists/*
-
-pip -q install --upgrade pip
-pip -q install --cache-dir=.pip -r requirements.txt
-
 uvicorn main:app --port "${BACKEND_PORT}" --host 0.0.0.0

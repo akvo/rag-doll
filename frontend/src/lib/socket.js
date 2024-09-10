38a3c031653a19de6ee70d66ee5f2564432bd913
wayangalihpratama <wgprtm@gmail.com>
2024-09-10 15:06:38 +0800

    [#85] Fix error when build next

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -2,22 +2,26 @@
 
 import { io } from "socket.io-client";
 
-const { hostname, origin } = window.location;
-const isLocalhost = hostname === "localhost";
+let socket;
 
-const transports = isLocalhost
-  ? ["polling", "websocket"]
-  : ["websocket", "polling"];
+if (typeof window !== "undefined") {
+  const { hostname, origin } = window.location;
+  const isLocalhost = hostname === "localhost";
 
-const socket = io(origin, {
-  path: "/api/socket.io",
-  transports: transports,
-  reconnection: true,
-  reconnectionAttempts: 10,
-  reconnectionDelay: 5000,
-  pingInterval: 25000, // 25 seconds
-  pingTimeout: 5000, // 5 seconds
-  upgrade: isLocalhost ? false : true,
-});
+  const transports = isLocalhost
+    ? ["polling", "websocket"]
+    : ["websocket", "polling"];
+
+  socket = io(origin, {
+    path: "/api/socket.io",
+    transports: transports,
+    reconnection: true,
+    reconnectionAttempts: 10,
+    reconnectionDelay: 5000,
+    pingInterval: 25000, // 25 seconds
+    pingTimeout: 5000, // 5 seconds
+    upgrade: isLocalhost ? false : true,
+  });
+}
 
 export default socket;

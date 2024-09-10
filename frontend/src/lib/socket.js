f9b5d33954d730aaccea872a5971bbf1b9bf5ff0
wayangalihpratama <wgprtm@gmail.com>
2024-09-10 14:26:17 +0800

    [#78] Update socket io client

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,12 +1,23 @@
+"use client";
+
 import { io } from "socket.io-client";
 
-const socket = io({
+const { hostname, origin } = window.location;
+const isLocalhost = hostname === "localhost";
+
+const transports = isLocalhost
+  ? ["polling", "websocket"]
+  : ["websocket", "polling"];
+
+const socket = io(origin, {
   path: "/api/socket.io",
+  transports: transports,
   reconnection: true,
   reconnectionAttempts: 10,
   reconnectionDelay: 5000,
   pingInterval: 25000, // 25 seconds
   pingTimeout: 5000, // 5 seconds
+  upgrade: isLocalhost ? false : true,
 });
 
 export default socket;

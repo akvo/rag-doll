102f65e4c24479deb4e734d7ca70cec9c2b334b6
wayangalihpratama <wgprtm@gmail.com>
2024-09-13 13:23:23 +0800

    [#91] Handle show media from chat history

---
+++ frontend/src/components/chats/ChatMedia.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+const ChatMedia = ({ type, url }) => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (url) => {
+    setSelectedImage(url); // Open modal with selected image
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null); // Close modal
+  };
+
+  const handleModalClick = (e) => {
+    // Close the modal if clicked outside the image (on the background)
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  };
+
+  if (type.includes("image")) {
+    return (
+      <>
+        <div className="mt-2">
+          <Image
+            width={225}
+            height={175}
+            src={url}
+            alt={url}
+            className="rounded-md shadow-sm mb-1 cursor-pointer"
+            onClick={() => handleImageClick(url)} // Handle click to show modal
+          />
+        </div>
+        {/* Image Modal */}
+        {selectedImage && (
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
+            onClick={handleModalClick} // Close modal when clicking outside the image
+          >
+            <div className="relative w-full">
+              <Image
+                width={375}
+                height={250}
+                layout="responsive"
+                src={selectedImage}
+                alt="Full Image"
+                className="max-w-full max-h-full"
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+            </div>
+            <button
+              className="absolute top-2 right-4 text-white text-lg"
+              onClick={closeModal} // Close modal with button click
+            >
+              ✕
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }
+
+  return null;
+};
+
+export default ChatMedia;

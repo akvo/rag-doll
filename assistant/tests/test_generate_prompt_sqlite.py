cfbe71df741b27b26294bbe36aee57991da7f46f
wayangalihpratama <wgprtm@gmail.com>
2024-10-10 12:25:50 +0800

    [#108] Add more test case

--- assistant/tests/test_generate_prompt_sqlite.py
+++ assistant/tests/test_generate_prompt_sqlite.py
@@ -5,10 +5,11 @@ import pandas as pd
 from seeder.generate_prompt_sqlite import main
 
 
-def test_generate_csv():
-    main(prefix="_test")
+db_path = "./db/assistant_test.sqlite"
 
-    db_path = "./db/assistant_test.sqlite"
+
+def test_generate_sqlite():
+    main(prefix="_test")
     assert os.path.exists(db_path)
 
     # Check that the SQLite file contains the correct data
@@ -20,4 +21,16 @@ def test_generate_csv():
     assert "en" in df["language"].tolist()
     assert "fr" in df["language"].tolist()
     assert "sw" in df["language"].tolist()
+
+
+def query_sqlite_by_lang():
+    conn = sqlite3.connect(db_path)
+    df = pd.read_sql("SELECT * FROM prompt WHERE language == 'en'", conn)
+    assert len(df) == 1
+    assert df["language"].tolist() == ["en"]
+    conn.close()
+
+
+def test_remove_sqlie():
     os.remove(db_path)
+    assert os.path.exists(db_path) is False

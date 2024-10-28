e6a8fb792dbbc0db7e00cad300951bb2223ca026
wayangalihpratama <wgprtm@gmail.com>
2024-10-28 15:13:42 +0800

    [#117] Remove env local setup

--- frontend/prod.sh
+++ frontend/prod.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-env | grep -E '^(FRONTEND_PORT|BACKEND_PORT)' > .env
-env | grep -E '^(NEXT_PUBLIC_VAPID_PUBLIC_KEY)' >> .env
-env | grep -E '^(NEXT_PUBLIC_VAPID_PRIVATE_KEY)' >> .env
-
 yarn build
 
 # Other commands (e.g copy read the docs files, env vars, etc)

28e7cc5dc2576522a289994a302a427c60f27969
wayangalihpratama <wgprtm@gmail.com>
2024-10-28 14:47:47 +0800

    [#117] Fix vapid key

--- frontend/prod.sh
+++ frontend/prod.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 env | grep -E '^(FRONTEND_PORT|BACKEND_PORT)' > .env
-env | grep -E 'NEXT_PUBLIC_VAPID_PUBLIC_KEY' >> .env
-env | grep -E 'NEXT_PUBLIC_VAPID_PRIVATE_KEY' >> .env
+env | grep -E '^(NEXT_PUBLIC_VAPID_PUBLIC_KEY)' >> .env
+env | grep -E '^(NEXT_PUBLIC_VAPID_PRIVATE_KEY)' >> .env
 
 yarn build
 

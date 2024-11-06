4c20ab35b1a2aa44258f8ba0c7a93615dd07ac8d
wayangalihpratama <wgprtm@gmail.com>
2024-11-06 18:14:20 +0800

    [#130] Unregister service worker when logged out

--- frontend/src/lib/cookies.js
+++ frontend/src/lib/cookies.js
@@ -17,5 +17,24 @@ export const getCookie = (name) => {
 };
 
 export const deleteCookie = (name) => {
+  // Unregister service worker
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .getRegistrations()
+      .then((registrations) => {
+        for (let registration of registrations) {
+          registration.unregister().then((success) => {
+            if (success) {
+              console.log("Service Worker unregistered");
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error unregistering service worker:", error);
+      });
+  }
+  // EOL unregister service worker
+
   document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; SameSite=Strict;`;
 };

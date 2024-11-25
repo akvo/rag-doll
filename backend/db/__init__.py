97d3e52b23a2695f0406583e2077866c8e33f8a5
wayangalihpratama <wgprtm@gmail.com>
2024-11-25 13:38:03 +0800

    [#133] Init conversation reconnect template env

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,1 +1,1 @@
-from .crud_chat import add_media, get_last_message, check_24h_window  # noqa
+from .crud_chat import add_media, check_24h_window  # noqa
--- backend/db/crud_chat.py
+++ backend/db/crud_chat.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from sqlmodel import Session, select
-from models import Chat, Chat_Media, Sender_Role_Enum, Chat_Session
+from models import Chat, Chat_Media, Sender_Role_Enum
 
 
 tz = timezone.utc
@@ -15,28 +15,18 @@ def add_media(session: Session, chat: Chat, media: list[dict]):
     session.commit()
 
 
-def get_last_message(session: Session, chat: Chat):
+def check_24h_window(session: Session, chat_session_id: int):
+    current_time = datetime.now(tz)
     last_message = session.exec(
         select(Chat)
-        .where(Chat.chat_session_id == chat.id)
+        .where(Chat.chat_session_id == chat_session_id)
         .where(
             Chat.sender_role != Sender_Role_Enum.ASSISTANT,
         )
         .order_by(Chat.created_at.desc(), Chat.id.desc())
     ).first()
-    return last_message
-
-
-def check_24h_window(session: Session):
-    # TODO :: run as backgorund task
-    # We also need to add an option for farmer
-    # to choose to stop receive automated message
-    current_time = datetime.now(tz)
-    chats = session.exec(select(Chat_Session)).all()
-    for chat in chats:
-        last_message = get_last_message(session=session, chat=chat)
-        # add UTC format to created_at
-        time_diff = current_time - last_message.created_at.replace(tzinfo=tz)
-        if time_diff >= timedelta(hours=24):
-            # TODO :: send_template_message here
-            print(last_message.message, chat.id)
+    # add UTC format to created_at
+    time_diff = current_time - last_message.created_at.replace(tzinfo=tz)
+    if time_diff >= timedelta(hours=24):
+        return True
+    return False
